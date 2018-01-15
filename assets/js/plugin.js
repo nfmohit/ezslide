220dba9eb4198e5f67f4c7b24b6a5842ef5ded36
Nahid Ferdous Mohit <nfmohit49@gmail.com>
2018-01-15 09:59:23 +0600

    Added loop functionality

--- assets/js/plugin.js
+++ assets/js/plugin.js
@@ -2,11 +2,21 @@
   * Navigation
   */
 $(".ezslide-wrapper .arrows > .prev").on("click", function(){
-    $('.ezslide').find('li.current').prev().addClass('current');
-    $('.ezslide').find('li.current').next().removeClass('current');
+	if ($('.ezslide').find('li.current').prev().length) {
+    	$('.ezslide').find('li.current').prev().addClass('current');
+    	$('.ezslide').find('li.current').next().removeClass('current');
+	} else {
+		$('.ezslide li').last().addClass('current');
+		$('.ezslide li').first().removeClass('current');
+	}	
 });
 
 $(".ezslide-wrapper .arrows > .next").on("click", function(){
-    $('.ezslide').find('li.current').next().addClass('current');
-    $('.ezslide').find('li.current').prev().removeClass('current');
+	if ($('.ezslide').find('li.current').next().length) {
+    	$('.ezslide').find('li.current').next().addClass('current');
+    	$('.ezslide').find('li.current').prev().removeClass('current');
+	} else {
+		$('.ezslide li').first().addClass('current');
+		$('.ezslide li').last().removeClass('current');
+	}
 });
